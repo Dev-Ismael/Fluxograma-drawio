3693538c6e265bf7259eae55c34a3b5e6a4aff5b
fic <fic@NOTE25-S28>
2025-09-01 21:52:19 -0300

    JOIN DML

--- Banco de dados/EcommerceDML.sql
+++ Banco de dados/EcommerceDML.sql
@@ -1,3 +1,4 @@
+
 -- DML (LINGUAGEM DE MANIPULACAO DE DADOS)
 -- INSERT (Cadastrar)
 -- UPDATE (Atualizar)
@@ -12,3 +13,38 @@ INSERT INTO ecommerce.cliente(nome_completo, email, senha, telefone, data_cadast
 ('Ismael', 'ismael@gmail.com', 'abc123', '11912345678', '2025-08-28'),
 ('Leo', 'leo@gmail.com', 'def456', '11987654321', '2025-08-28')
 
+INSERT INTO ecommerce.pedido(id_cliente, data_pedido, valor_total, status) VALUES 
+( 1, '01/09/2025', 100, 'Andamento')
+
+
+-- SELECT
+SELECT nome, idade, cpf
+FROM clinica.paciente;
+
+SELECT * FROM clinica.medico;
+
+-- JOIN(Juntar)
+-- Com o JOIN posso juntar tabelas
+-- Utilizando a chave Estrangeira
+
+-- Listar, Data, Valor, Nome do medico
+SELECT 
+	data,
+	valor,
+	nome
+FROM clinica.consulta AS cs
+JOIN 
+	clinica.medico AS med
+ON clinica.consulta.id_medico = 
+med.id_medico
+
+
+
+-- EXERCICIOS ECOMMERCE
+SELECT  ec.nome_completo,
+		ep.id_pedido
+FROM ecommerce.cliente AS ec
+JOIN ecommerce.pedido AS ep
+ON ep.id_cliente = ec.id_cliente
+
+		
