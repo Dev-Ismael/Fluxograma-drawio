984edd498e4fefb0c8151a1085d055c339c4f516
fic <fic@NOTE25-S28>
2025-09-03 21:53:05 -0300

    API em andamento

---
+++ API/api/api/src/main/java/br/com/ecommerce/api/service/ClienteServices.java
@@ -0,0 +1,27 @@
+package br.com.ecommerce.api.service;
+
+import br.com.ecommerce.api.model.Cliente;
+import br.com.ecommerce.api.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service // Avisa o SPRING que e um servico
+public class ClienteServices {
+    // Injecao de dependencia
+    // Falar que services depende de alguem
+    // fianl - constante
+    private final ClienteRepository clienteRepository;
+
+    public ClienteServices(ClienteRepository repo) {
+        clienteRepository = repo;
+    }
+
+    // Metodo de Listar todos os cliente
+    // List e melhor que array pq nao tem limite
+    // Vetor nao e uma lista, pois possui limite
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+}
